98a110c73e5576293f7ffc1fc80c6dfd41180681
DeskRabbit11 <tannerk@iastate.edu>
2017-04-07 19:19:19 -0500

    Update managerPrinter.java

--- managerPrinter.java
+++ managerPrinter.java
@@ -1,12 +1,17 @@
 	import java.io.*;
-	
-
+	import java.util.Scanner;
+	/**
+ * Printer class for manager's to print checks, reports, and W2s.
+ * @author Tanner Koolstra
+ *
+ */
 public class managerPrinter {
 
 	    public static void main(String[] args)
 	    {
-	    	System.out.println("Please enter file type(check, report, or W2): ");
-	    	System.out.println("Please enter file name: ");
+	    	Scanner console = new Scanner(System.in);
+	    	System.out.print("Please enter file type(check, report, or W2): ");
+	    	System.out.print("Please enter file name: ");
 	    	
 	        File check = new File("C:/Users/fileLocation.txt");
 	        File report = new File("C:/Users/fileLocation.txt");
@@ -29,8 +34,13 @@ public class managerPrinter {
 	            if ( printWriter != null ) 
 	            {
 	                printWriter.close();
+	                printWriter.close();
+	                printWriter.close();
+	                console.close();
+	                
 	            }
 	        }
 	    }
 	}
+
 
