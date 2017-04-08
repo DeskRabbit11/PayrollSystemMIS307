1d0956cbbb93a514893939223fc4550d8ab9e5de
DeskRabbit11 <tannerk@iastate.edu>
2017-04-07 19:21:32 -0500

    Update employeePrinter.java

--- employeePrinter.java
+++ employeePrinter.java
@@ -1,14 +1,17 @@
-	import java.io.*;
-/**
- * Printer class for employee's to print their payslip copies.
- * @author Tanner Koolstra
- *
- */
+import java.io.*;
+	import java.util.Scanner;
+	/**
+	 * Printer class for employee's to print their payslip copies.
+	 * @author Tanner Koolstra
+	 *
+	 */
 public class employeePrinter {
 
 	    public static void main(String[] args) 
 	    {
-
+	    	Scanner console = new Scanner(System.in);
+	    	System.out.print("Please enter payslip file name: ");
+	    	
 	        File payslipCopy = new File("C:/Users/fileLocation.txt");
 
 	        PrintWriter printWriter = null;
@@ -26,6 +29,7 @@ public class employeePrinter {
 	            if ( printWriter != null ) 
 	            {
 	                printWriter.close();
+	                console.close();
 	            }
 	        }
 	    }
